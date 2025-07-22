6c6c01b639e1416e6943adf60cad36035241dcfe
amakhoekars@gmail.com <amakhoekars@gmail.com>
2025-07-22 12:44:13 -0400

    edit header

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -18,7 +18,8 @@ export const Hero = () => {
      {/* <div className="absolute top-0 left-0 w-full h-full bg-[radial-gradient(ellipse_100%_100%_at_bottom_left,#20a08b,#10b07b_66%)]"/>*/}
          <Image src={b1} className="w-full h-full object-cover" alt="backgoround image"/>
         <div className="absolute mb-2 top-0 my-8 w-full h-full flex flex-col items-center justify-center">  
-          <a className="border p1 mb-4 border-white py-1 px-3 gap-3 rounded-lg text-white inline-flex">Inovacao em alta</a>      
+          <a className="border p1 mb-4 border-white py-1 px-3 gap-3 rounded-lg text-white inline-flex">
+            <span className="text-white border-blue-400 rounded-lg border">Vhonani +</span>Inovacao em alta</a>      
             <h1 className="relative z-10 text-6xl md:text-7xl lg:text-8xl tracking-wide bg-clip-text text-transparent bg-gradient-to-b from-blue-100 to-[#f2f4f5] text-center t1 font-sans font-medium">
           We design <br/><span className="bg-clip-text text-transparent bg-gradient-to-b from-blue-500 to-[#f2f4f5]">the future</span>
         </h1>
--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -37,21 +37,25 @@ export const LogoTicker = () => {
 
         <div className="relative mx-auto max-w-5xl text-center">
             <span className="text-gray-700 my-3 flex items-center justify-center font-medium uppercase tracking-wider">
-            Why choose us
+            Porque Vhonani Tech
             </span>
             <h2
                 className="block w-full bg-gradient-to-b from-black to-gray-400 bg-clip-text font-bold text-transparent text-3xl sm:text-4xl">
-                Build a Website That Your Customers Love
+                Desenvolvemos solucoes a medida
             </h2>
             <p
                 className="mx-auto my-4 w-full max-w-xl bg-transparent text-center font-medium leading-relaxed tracking-wide text-gray-800">
-                Our templates allow for maximum customization. No technical skills required – our intuitive design tools
-                let
-                you get the job done easily.
+                Somos uma startup inclusiva, que tem como foco o desenvolvimento de equipamento de suporte audiovisual, para pessoas com deficiencia.
             </p>
         </div>
     </section>
-
+<div className="mt-16 flex flex-col gap-4">
+  {stats.map((title,desc) => (
+    <div key={title} className="border border-white/30 px-5 py-10 text-center">
+      <h3>{title}</h3>
+      </div>
+  ))}
+  </div>
    {/*<div className="absolute bottom-0 left-0 z-0 h-1/3 w-full border-b z12"/> */} 
     {/* <div className="absolute bottom-0 right-0 z-0 h-1/3 w-full z13"/>*/}    
 </div>
